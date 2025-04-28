4b9a13b0dc58d09f270e9dd78acbc76737cfae8f
Porcup13 <23521064@gm.uit.edu.vn>
2025-04-28 23:32:14 +0700

    đã thêm unbreakablebrick để làm background cho các viên k hủy đc

--- BackgroundColor.h
+++ BackgroundColor.h
@@ -6,8 +6,8 @@ class CBackgroundColor : public CGameObject
 {
 	int width;				// Unit: cell 
 	int height;				// Unit: cell 
-	int color; // 0: Blue, 1: Black, 2:mushroom, 3:flower, 4:star, 5:smallbush
-	int colorSpriteId[6] = { 170004, 170005, 170001, 170002, 170003, 130001 };
+	int color; // 0: Blue, 1: Black, 2:mushroom, 3:flower, 4:star, 5:smallbush, 6:halfblack
+	int colorSpriteId[7] = { 170004, 170005, 170001, 170002, 170003, 130001, 120001 };
 public:
 	CBackgroundColor(float x, float y,int width, int height, int color) : CGameObject(x, y) {
 		this->width = width;
