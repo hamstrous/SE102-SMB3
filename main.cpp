095c7d20e41ea9dab567638af9a6b7735af70baf
23521093 <156832544+hamstrous@users.noreply.github.com>
2025-05-24 14:23:22 +0700

    Tắt thư viện và define crt debug

--- main.cpp
+++ main.cpp
@@ -226,7 +226,7 @@ int WINAPI WinMain(
 	SetWindowPos(hWnd, 0, 0, 0, SCREEN_WIDTH*3, SCREEN_HEIGHT*3, SWP_NOMOVE | SWP_NOOWNERZORDER | SWP_NOZORDER);
 
 	Run();
-	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
-	_CrtDumpMemoryLeaks();
+	//_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+	//_CrtDumpMemoryLeaks();
 	return 0;
 }
